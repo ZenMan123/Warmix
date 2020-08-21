2a89a23c63f2ec1b7a71e009761d71edf37934fe
ZenMan67 <artemsafin67@yandex.ru>
2020-08-21 15:25:48 +0500

    Both melee and pistol warriors can attack now

--- warriors/melee_warrior.py
+++ warriors/melee_warrior.py
@@ -1,7 +1,10 @@
 from .base_warrior import BaseWarrior
-from configurations.size_configurations import ANIMATIONS_COUNT
+from configurations.size_configurations import ANIMATIONS_COUNT, WARRIOR_WIDTH
+from configurations.modes_configuration import LEFT, RIGHT
 from services_for_game.camera import Camera
 from game.game import Game
+
+import pygame
 
 
 class MeleeWarrior(BaseWarrior):
@@ -13,8 +16,24 @@ class MeleeWarrior(BaseWarrior):
         if self.conditions['attack']['attack_count'] == ANIMATIONS_COUNT - 1:
             self.deactivate('attack')
             self.conditions['attack']['attack_count'] = -1
+
+            self.rect.y = self.rect.y - WARRIOR_WIDTH // 5 if LEFT else self.rect.y + WARRIOR_WIDTH // 5
+
+            res = pygame.sprite.spritecollide(self, self.game.warriors, False)
+            if res:
+                for obj in res:
+                    if obj == self:
+                        continue
+                    else:
+                        obj.receive_damage(self.power)
+                        break
+
+            self.rect.y = self.rect.y + WARRIOR_WIDTH // 5 if LEFT else self.rect.y - WARRIOR_WIDTH // 5
         else:
             self.conditions['attack']['attack_count'] += 1
+
+
+
 
 
 
